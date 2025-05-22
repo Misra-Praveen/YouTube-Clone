bfa76514c0f048e4f3f5b9233a47a8465f308e29
Misra-Praveen <vgipra@gmail.com>
2025-05-22 16:38:24 +0530

    updated server page with all routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
-import express from "express"
-import mongoose from "mongoose"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoute.js";
+import channelRoutes from "./routes/channelRoute.js";
+import videoRoutes from "./routes/videoRoute.js";
+import commentRoutes from "./routes/commentRoute.js";
 
 
 
-mongoose.connect("mongodb+srv://praveen:Reaction111@cluster0.c35enxr.mongodb.net/"); //connect to mongodb atlas database
+
+const PORT = 5000;
+mongoose.connect("mongodb://localhost:27017/youtube"); //connect to mongodb database
 //all the code below is just to see if the connection is working or not
 const db=mongoose.connection;
 db.on("open", ()=>{
@@ -15,8 +23,16 @@ db.on("error", (err)=>{
 
 const app=new express();// initialize the Express app
 //make a server
-app.listen(5000, ()=>{
-    console.log("server is running on port 5100");
+app.listen(PORT, ()=>{
+    console.log("server is running on port 5000");
 });
 
+// Middleware
+app.use(cors());
 app.use(express.json()); //to parse the incoming json
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/channels", channelRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
