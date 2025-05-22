df8d70f5b0e1d3853f2a02dc1a10a8c623b0e79a
Misra-Praveen <vgipra@gmail.com>
2025-05-22 19:10:25 +0530

    updated main and App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
+import React, {useState} from 'react'
+import Header from './components/Header'
+import Sidebar from "./components/Sidebar"
+import { Outlet } from "react-router-dom";
 
 
-import './App.css'
 
-function App() {
- 
+const App = () => {
+const [sidebarOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+  console.log("Sidebar is open?", sidebarOpen);
 
   return (
-    <>
-      <h1>App Component</h1>
-    </>
+    
+    <div className="flex flex-col min-h-screen">
+      {/* Top Navigation */}
+      <Header toggleSidebar={toggleSidebar} />
+
+      <div className="flex flex-1 pt-16">
+        {/* Sidebar */}
+        <Sidebar isOpen={sidebarOpen} />
+
+        {/* Main content area */}
+        <main className="flex-1 p-4 overflow-y-auto ml-0">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,57 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import store  from "./redux/store";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+const Login = lazy(() => import("./pages/Login.jsx"));
+const Register = lazy(() => import("./pages/Register.jsx"));
+const Home = lazy(() => import("./pages/Home.jsx"));
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Suspense fallback="Loading..">
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/login",
+        element: (
+          <Suspense fallback="Loading..">
+            <Login />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/register",
+        element: (
+          <Suspense fallback="Loading..">
+            <Register />
+          </Suspense>
+        ),
+      },
+      
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+     
+      <RouterProvider router={appRouter} />
+      
+    </Provider>
+  </StrictMode>
+);
